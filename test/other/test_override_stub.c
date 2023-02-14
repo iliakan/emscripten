d0694ff885f4572fc96ec912d8573f61e2e6bfd1
Ilya Kantor <iliakan@users.noreply.github.com>
2023-02-14 17:57:48 +0100

    minor fixes

---
+++ test/other/test_override_stub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <emscripten.h>
+
+pid_t fork() {
+  return 1;
+}
+
+int main() {
+  printf("%d\n", fork()); // re-implemented call
+  chroot(NULL); // stubbed call
+  return 0;
+}
